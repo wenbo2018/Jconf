9e996acedfe5ddf82c6eeb40b3646e66f4c4f1c0
‘wenbo2018’ <wenboing.shen@gmail.com>
2017-05-02 08:21:40 +0800

    fix bug

---
+++ jconf-web/src/main/java/com/github/wenbo2018/jconf/web/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.github.wenbo2018.jconf.web.bean;
+
+/**
+ * Created by wenbo.shen on 2017/5/1.
+ */
+public class Constants {
+}
---
+++ jconf-web/src/main/java/com/github/wenbo2018/jconf/web/constants/ReturnCode.java
@@ -0,0 +1,7 @@
+package com.github.wenbo2018.jconf.web.constants;
+
+/**
+ * Created by wenbo.shen on 2017/5/1.
+ */
+public class ResultCode {
+}
